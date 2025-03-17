b0848c45c33b5022e699fcb8a66b6f8425fede79
Roman Campbell <Roman_Campbell@lawb.uscourts.gov>
2025-03-17 11:53:40 -0500

    Sidebar update-functional

--- printerdashboard.client/src/App.tsx
+++ printerdashboard.client/src/App.tsx
@@ -1,21 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import AppSidebar from "./dashboard/AppSidebar";
 import { useState } from 'react';
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
+import { Box, CssBaseline, Theme, ThemeProvider } from "@mui/material";
+import { darkTheme } from "./themes";
 
 export const App = () => {
-    const [theme, colorMode] = useMode();
     const [isSidebar, setIsSidebar] = useState(true);
 
     return (
-        <ColorModeContext.Provider value={colorMode}>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <div className="app">
-                    <AppSidebar />
-                </div>
-            </ThemeProvider>
-        </ColorModeContext.Provider>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Box
+                sx={{
+                    backgroundColor: (theme: Theme) => {
+                        return theme.palette.background.default;
+                    },
+                    minHeight: "100vh", // ensures the background covers the full viewport height
+                }}
+            >
+                <AppSidebar />
+            </Box>
+        </ThemeProvider>
     );
 }
--- printerdashboard.client/src/dashboard/AppSidebar.tsx
+++ printerdashboard.client/src/dashboard/AppSidebar.tsx
@@ -1,161 +1,157 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Box, IconButton, useTheme, Typography } from "@mui/material";
-import { useContext, useState } from "react";
+import { useContext, useState, FunctionComponent } from "react";
 import BKSeal from "../assets/seal_no_border.png";
 import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
 import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import FolderSharpIcon from '@mui/icons-material/FolderSharp';
+import HomeIcon from '@mui/icons-material/Home';
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import { ColorModeContext, tokens } from "../theme";
 import { Link } from "react-router-dom";
-import { Sidebar, Menu, MenuItem, sidebarClasses, MenuItemStyles, menuClasses } from "react-pro-sidebar";
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import React from "react";
+import { darkTheme } from "../themes";
+import { SvgIconProps } from "@mui/material/SvgIcon";
+import Icon from "@mui/icons-material/icon";
+import { SidebarHeader } from "../components/SidebarHeader";
 
-type Theme = 'dark';
-
-const themes = {
-    dark: {
-        sidebar: {
-            backgroundColor: '#0b2948',
-            color: '#8ba1b7',
-        },
-        menu: {
-            menuContent: '#082440',
-            icon: '#59d0ff',
-            hover: {
-                backgroundColor: '#00458b',
-                color: '#b6c8d9',
-            },
-            disabled: {
-                color: '#3e5e7e',
-            },
-        },
-    },
-};
-
-// hex to rgba converter
-const hexToRgba = (hex: string, alpha: number = 1) => {
-    const r = parseInt(hex.slice(1, 3), 16);
-    const g = parseInt(hex.slice(3, 5), 16);
-    const b = parseInt(hex.slice(5, 7), 16);
-
-    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
-};
-
-const Item = ({ title, to, icon, selected, setSelected }) => {
-    const theme = useTheme();
-    const colors = tokens();
-    return (
-        <MenuItem
-            active={selected === title}
-            style={{
-                color: colors.grey[100],
-            }}
-            onClick={() => setSelected(title)}
-            icon={icon}
-        >
-            <Typography>{title}</Typography>
-            <Link to={to} />
-        </MenuItem>
-    );
-};
+interface ItemProps {
+    title: string;
+    to: string;
+    icon: React.ComponentType<SvgIconProps>;
+    selected: string;
+    setSelected: (value: string) => void;
+}
 
 const AppSidebar = () => {
-    //const theme = useTheme();
-    const colors = tokens();
     const [isCollapsed, setIsCollapsed] = useState(false);
     const [toggled, setToggled] = useState(false);
     const [selected, setSelected] = useState("Dashboard");
-    const [theme, setTheme] = useState<Theme>('dark');
-
-    const menuItemStyles: MenuItemStyles = {
-        root: {
-            fontSize: '13px',
-            fontWeight: 400,
-        },
-        icon: {
-            color: themes[theme].menu.icon,
-            [`&.${menuClasses.disabled}`]: {
-                color: themes[theme].menu.disabled.color,
-            },
-        },
-        SubMenuExpandIcon: {
-            color: '#b6b7b9',
-        },
-        subMenuContent: ({ level }) => ({
-            backgroundColor:
-                level === 0
-                    ? hexToRgba(themes[theme].menu.menuContent)
-                    : 'transparent',
-        }),
-        button: {
-            [`&.${menuClasses.disabled}`]: {
-                color: themes[theme].menu.disabled.color,
-            },
-            '&:hover': {
-                backgroundColor: hexToRgba(themes[theme].menu.hover.backgroundColor, !isCollapsed ? 0.8 : 1),
-                color: themes[theme].menu.hover.color,
-            },
-        },
-        label: ({ open }) => ({
-            fontWeight: open ? 600 : undefined,
-        }),
-    };
 
     return (
-        <div style={{ display: 'flex', height: '100%' }}>
-            <Sidebar
-                collapsed={isCollapsed}
-                toggled={toggled}
-                onBackdropClick={() => setToggled(false)}
-                breakPoint="md"
-                backgroundColor={hexToRgba(themes[theme].sidebar.backgroundColor)}
-                rootStyles={{
-                    color: themes[theme].sidebar.color,
-                }}
-            >
-                <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-                    <Menu>
-                        <MenuItem
-                            onClick={() => setIsCollapsed(!isCollapsed)}
-                            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
-                            style={{
-                                margin: "10px 0 20px 0",
-                                color: colors.grey[500],
+        <Box
+            sx={{
+                "& .pro-sidebar-inner": {
+                    background: `${darkTheme.palette.secondary.main} !important`,
+                },
+                "& .pro-icon-wrapper": {
+                    backgroundColor: "transparent !important",
+                },
+                "& .pro-inner-item": {
+                    padding: "5px 35px 5px 20px !important",
+                    color: darkTheme.palette.secondary.main + " !important", // set desired text color
+                },
+                "& .pro-inner-item:hover": {
+                    color: "#868dfb !important",
+                },
+                "& .pro-menu-item.active": {
+                    color: "#6870fa !important",
+                },
+                "& .pro-sub-menu-content": {
+                    backgroundColor: darkTheme.palette.secondary.main + " !important",
+                },
+                "& .pro-submenu-item": {
+                    color: "lightgray !important"
+                }
+            }}
+        >
+            <Box style={{ display: 'flex', height: '100vh' }}>
+                <Sidebar
+                    collapsed={isCollapsed}
+                    toggled={toggled}
+                    onBackdropClick={() => setToggled(false)}
+                    breakPoint="md"
+                    backgroundColor={darkTheme.palette.secondary.main}
+                >
+                    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+                        <Menu
+                            menuItemStyles={{
+                                button: {
+                                    backgroundColor: darkTheme.palette.secondary.main,
+                                    '&:hover': {
+                                        backgroundColor: 'green'
+                                    }
+                                },
+                                SubMenuExpandIcon: {
+                                    color: darkTheme.palette.primary.main,
+                                }
+
                             }}
                         >
+                            <MenuItem
+                                onClick={() => setIsCollapsed(!isCollapsed)}
+                                icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+
+                            >
+                                {!isCollapsed && (
+                                    <Box
+                                        display="flex"
+                                        justifyContent="space-between"
+                                        alignItems="center"
+                                        ml="15px"
+                                    >
+                                        <Typography variant="h4">
+                                            Printer Dashboard
+                                        </Typography>
+                                        <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                                            <MenuOutlinedIcon />
+                                        </IconButton>
+                                    </Box>
+                                )}
+                            </MenuItem>
+
                             {!isCollapsed && (
-                                <Box
-                                    display="flex"
-                                    justifyContent="space-between"
-                                    alignItems="center"
-                                    ml="15px"
-                                >
-                                    <Typography variant="h6" color={colors.grey[100]}>
-                                        PRINTER DASHBOARD
-                                    </Typography>
-                                    <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
-                                        <MenuOutlinedIcon />
-                                    </IconButton>
+                                <Box mb="25px">
+                                    <Box display="flex" justifyContent="center" alignItems="center">
+                                        <img
+                                            src={BKSeal}
+                                            style={{ width: "100px", height: "100px" }}
+                                        />
+                                    </Box>
                                 </Box>
                             )}
-                        </MenuItem>
 
-                        {!isCollapsed && (
-                            <Box mb="25px">
-                                <Box display="flex" justifyContent="center" alignItems="center">
-                                    <img
-                                        src={BKSeal}
-                                        style={{ width: "100px", height: "100px" }}
-                                    />
-                                </Box>
+                            <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+                                {/*************************************************************************/}
+                                {/* DASHBOARD */}
+                                {/*************************************************************************/}
+                                <MenuItem
+                                    style={{
+                                        color: darkTheme.palette.info.main
+                                    }}
+                                    icon={<HomeIcon />}
+                                ><Typography>Dashboard</Typography></MenuItem>
+
+                                {/*************************************************************************/}
+                                {/* CHARTS */}
+                                {/*************************************************************************/}
+                                <SubMenu
+                                    label="Charts"
+                                    icon={<BarChartOutlinedIcon />}
+                                >
+                                    <MenuItem> Pie charts</MenuItem>
+                                    <MenuItem> Line charts</MenuItem>
+                                    <MenuItem> Bar charts</MenuItem>
+                                </SubMenu>
+
+                                {/*************************************************************************/}
+                                {/* DATA TABLES */}
+                                {/*************************************************************************/}
+                                <SubMenu
+                                    label="Data Tables"
+                                    icon={<FolderSharpIcon />}
+                                >
+                                    <MenuItem>Printer Data</MenuItem>
+                                </SubMenu>
                             </Box>
-                        )}
-                    </Menu>
-                </div>
-            </Sidebar>
+                        </Menu>
+                    </div>
+                </Sidebar>
 
-        </div>
+            </Box>
+        </Box>
     )
 }
 
